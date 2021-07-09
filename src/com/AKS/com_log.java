7248dee68ebae990909812aaef456c82fcdf1e92
iaman-singh <amankumar.singh2020b@vitstudent.ac.in>
2021-07-09 16:03:52 +0530

    comparison and logical operators  in JAVA

---
+++ src/com/AKS/com_log.java
@@ -0,0 +1,27 @@
+package com.AKS;
+
+public class com_log {
+    public static void main(String[] args){
+        // a == b
+        // a != b
+        // a < b
+        // a > b
+        // a <= b
+        // a >= b
+        boolean isSunUP = true;
+        if(isSunUP == true)
+            System.out.println("day");
+        else
+            System.out.println("night");
+        // && --> and
+        int a = 50;
+        int b = 40;
+        if (a < 60 && b < 60)
+            System.out.println("both less than 60");
+        // || --> or
+        if (a < 50 || b < 50)
+            System.out.println("atleat one of them is less than 50");
+        // ! --> not
+
+    }
+}
