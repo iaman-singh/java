ea997a13d63c2579efe0e8e528faea8b3086ecf3
iaman-singh <amankumar.singh2020b@vitstudent.ac.in>
2021-07-09 16:04:53 +0530

    switch case

---
+++ src/com/AKS/switch_case.java
@@ -0,0 +1,20 @@
+package com.AKS;
+
+public class switch_case {
+    public static void main(String[] args){
+        int day = 1; // 1 -  monday, 2 -  tuesday and so on
+        switch(day){
+            case 1 :
+                System.out.println("monday");
+                break;
+            case 2 :
+                System.out.println("tuesday");
+                break;
+            default :
+                System.out.println("wed to sunday");
+
+        }
+
+
+    }
+}
